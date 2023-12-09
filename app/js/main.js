47805c54b4f4250dcafef6e84a43e60b180df0bd
Ella2235 <olga33486@gmail.com>
2023-12-09 16:26:22 +0800

    added corrected scss, made half interactive

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,19 @@
-const box = document.querySelector(".box");
-const btn = document.querySelector(".button");
+$(function () {
+  //   Fancybox.bind('[data-fancybox="gallery"]', {});
+
+  var stickyHeader = $(".menu");
+  var stickyContent = $(".header__inner");
+  var scrollChange = 1;
+
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= scrollChange) {
+      stickyHeader.addClass("menu__fixed");
+      stickyContent.addClass("inner__fixed");
+    } else {
+      stickyHeader.removeClass("menu__fixed");
+      stickyContent.removeClass("inner__fixed");
+    }
+  });
+});
