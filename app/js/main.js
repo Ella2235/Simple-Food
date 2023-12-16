b91862b080fa4eccc7ec213a4cf59349583b5795
Ella2235 <olga33486@gmail.com>
2023-12-17 00:36:52 +0800

    added made an interactive header, footer. section

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,16 @@
 $(function () {
-  var stickyHeader = $(".header");
+  var mixer = mixitup(".popular-category");
+
+  var stickyHeader = $(".header__inner");
   var scrollChange = 1;
 
   $(window).scroll(function () {
     var scroll = $(window).scrollTop();
 
     if (scroll >= scrollChange) {
-      stickyHeader.addClass("header__fixed");
+      stickyHeader.addClass("header__inner-fixed");
     } else {
-      stickyHeader.removeClass("header__fixed");
+      stickyHeader.removeClass("header__inner-fixed");
     }
   });
 });
